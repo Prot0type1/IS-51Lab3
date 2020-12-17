d0894e751b5e203476048eb96a33fd4853988185
Prot0type1 <solorio011@hotmail.com>
2020-12-17 15:00:04 -0800

    lab3

---
+++ IS51Lab3.py
@@ -0,0 +1,36 @@
+"""" 
+start
+
+
+create list to store 5 #'s as a float
+capture user's input grades
+each capture will be added to list
+ascend sort, then split the items in index 2
+the three highest will be used to sum them and divide by 3
+
+
+finalize
+
+"""
+
+grade = {}
+
+
+grade = input  ("Enter the 1st grade")
+grades.appeds(float(grade)) 
+grade = input  ("Enter the 2nd grade")
+grades.appeds(float(grade))
+grade = input  ("Enter the 3rd grade")
+grades.appeds(float(grade))
+grade = input  ("Enter the 4th grade")
+grades.appeds(float(grade))
+grade = input  ("Enter the 5th grade")
+grades.appeds(float(grade))
+
+grades = sort{}
+grades = grade(2:)
+grades = sum(grades)
+result = grades /3
+
+
+print("Average Grade {0:21)%$".format(resuit))
